3c7a97a75fb2da9a56b433018c42c66de41cab7e
Jpan <Japan8789@gmail.com>
2016-12-25 01:27:53 -0500

    Setting up default route

--- blog/config.py
+++ blog/config.py
@@ -1,5 +1,5 @@
 import os
 class DevelopmentConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://jpan@localhost:5432/blogful"
+    SQLALCHEMY_DATABASE_URI = "postgresql:///blogful"
     DEBUG = True
 
