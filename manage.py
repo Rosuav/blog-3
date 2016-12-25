eac1050c255557a1e443377f26e948b7204bc08f
Jpan <Japan8789@gmail.com>
2016-12-24 22:45:30 -0500

    Database and manager additions

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 import os
-from flask.ext.script import Manager
+from flask_script import Manager
+from loremipsum import get_sentences
 
 from blog import app
 
@@ -9,6 +10,16 @@ manager = Manager(app)
 def run():
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
+
+@manager.command
+def seed():
+	for i in range(25):
+		entry = Entry(
+			title = "Test Entry #{}".format(i),
+			content = get_sentences(1)
+		)
+		session.add(entry)
+	session.commit()
 
 if __name__ == "__main__":
     manager.run()
