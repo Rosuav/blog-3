6353642e4a27eb98f83609ff22b424b3bb68ad81
= <Japan8789@gmail.com>
2016-12-25 22:23:47 -0500

    Replacing loremipsum with Faker

--- manage.py
+++ manage.py
@@ -1,11 +1,11 @@
 import os
 from flask_script import Manager
-from loremipsum import get_sentences
-
+from faker import Faker
 from blog.database import session, Entry
 from blog import app
 
 manager = Manager(app)
+fake = Faker()
 
 @manager.command
 def run():
@@ -19,7 +19,7 @@ def seed():
 			title = "Test Entry #{}".format(i),
 			# get_sentences returns a tuple the size of
 			# what's passed in. [0] will access the tuple
-			content = get_sentences(1)[0]
+			content = fake.text()
 		)
 		session.add(entry)
 	session.commit()
