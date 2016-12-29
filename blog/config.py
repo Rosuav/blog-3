a9004d7c3d75839b8a1b8de881e8cbbdcd543943
= <Japan8789@gmail.com>
2016-12-29 10:48:59 -0800

    Command to add users to DB

--- blog/config.py
+++ blog/config.py
@@ -2,4 +2,4 @@ import os
 class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql:///blogful"
     DEBUG = True
-    SECRET_KEY = os.environ.get("RANDOM_KEY_TO_REPLACE", os.random(12))
+    SECRET_KEY = os.environ.get("RANDOM_KEY_TO_REPLACE", os.urandom(12))
--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 import os
 from flask_script import Manager
 from faker import Faker
-from blog.database import session, Entry
+from blog.database import session, Entry, User
 from blog import app
+from getpass import getpass
+from werkzeug.security import generate_password_hash
 
 manager = Manager(app)
 fake = Faker()
@@ -23,6 +25,23 @@ def seed():
 		)
 		session.add(entry)
 	session.commit()
+
+@manager.command
+def adduser():
+    name = input("Name: ")
+    email = input("Email: ")
+    if session.query(User).filter_by(email=email).first():
+        print("User with that email address already exists")
+        return
+
+    password = ""
+    while len(password) < 8 or password !=password2:
+        password=getpass("Password: ")
+        password2=getpass("Re-enter Password: ")
+
+    user = User(name=name, email=email, password=generate_password_hash(password))
+    session.add(user)
+    session.commit()
 
 if __name__ == "__main__":
     manager.run()
