37bc4ba0c77c33e907cb4557ea3629f56ca42c91
= <Japan8789@gmail.com>
2017-01-03 19:44:35 -0800

    New Testing Config

--- blog/config.py
+++ blog/config.py
@@ -3,3 +3,8 @@ class DevelopmentConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql:///blogful"
     DEBUG = True
     SECRET_KEY = os.environ.get("RANDOM_KEY_TO_REPLACE", os.urandom(12))
+
+class TestingConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql:///blogful-test"
+    DEBUG = False
+    SECRET_KEY = "Not Secret"
