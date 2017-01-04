74a0c941650540455f678d4125267625bae62a70
= <Japan8789@gmail.com>
2017-01-03 20:14:49 -0800

    New dummy unit tests

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -9,7 +9,14 @@ import blog
 from blog.filters import *
 
 class FilterTests(unittest.TestCase):
-    pass
+    def test_date_format(self):
+        date = datetime.date(1999,12,31)
+        formatted = dateformat(date, "%y/%m/%d")
+        self.assertEqual(formatted, "99/12/31")
+
+    def test_data_format_none(self):
+        formatted = dateformat(None, "%y/%m/%d")
+        self.assertEqual(formatted, None)
 
 if __name__ == "__main__":
     unittest.main()
