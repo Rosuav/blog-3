1b4bfd97076c583fb5002ff8140f7fc8ec145a36
= <Japan8789@gmail.com>
2017-01-09 16:21:22 -0500

    Created travis config file and renamed tests

--- blog/config.py
+++ blog/config.py
@@ -8,3 +8,8 @@ class TestingConfig(object):
     SQLALCHEMY_DATABASE_URI = "postgresql:///blogful-test"
     DEBUG = False
     SECRET_KEY = "Not Secret"
+
+class TravisConfig(object):
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/blogful-test"
+    DEBUG = False
+    SECRET_KEY = "Not Secret"
